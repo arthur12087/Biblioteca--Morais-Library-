0aa7c2b837737dd28fa8dced2735fa27c8197ac0
arthur12087 <arthurferare@gmail.com>
2020-11-21 23:45:52 -0300

    commit final
    finalização do projeto

--- Biblioteca/funcionalidadesBiblioteca.py
+++ Biblioteca/funcionalidadesBiblioteca.py
@@ -1,32 +1,228 @@
-informacoesLivro = []
+import json
+listaLivros = []
+livrosReservados = []
+
+
 def cadastroDeLivros():
+    livro = {}
 
     nomeLivro = str(input("Insira o nome/título do livro: "))
-    autorLivro = str(input("insira o nome do autor da obra/livro"))
-    identificacao = str(input("insira o ID do livro"))
-    numeroEdicao = str(input("informe o numero da edição do livro"))
-    livroAlugavel = str(input("Informe se este livro é alugável (S/N): "))
-    livroUnidades = int(input("Quantas unidades tem deste livro? "))
+    livro['titulo'] = nomeLivro
 
-    informacoesLivro.append(nomeLivro)
-    informacoesLivro.append(autorLivro)
-    informacoesLivro.append(identificacao)
-    informacoesLivro.append(numeroEdicao)
-    informacoesLivro.append(livroAlugavel)
-    informacoesLivro.append(livroUnidades)
+    autorLivro = str(input("insira o nome do autor da obra/livro:"))
+    livro['autorLivro'] = autorLivro
+
+    categoriaLivro = str(input("informe a categoria deste livro:"))
+    livro['categoria'] = categoriaLivro
+
+    tematicaLivro = str(input("informe a tematica deste livro:"))
+    livro['tematica'] = tematicaLivro
+
+    ano = str(input("insira o ano do livro:"))
+    livro['anoLivro'] = ano
+
+    livroAlugavel = str(input("Informe se este livro é alugável (S/N): "))
+    livro['livroAlugavel'] = livroAlugavel
+
+    livroUnidades = str(input("Quantas unidades tem deste livro? "))
+    livro['livroUnidades'] = livroUnidades
+
+    listaLivros.append(livro)
+
+    with open("livros.json","w") as json_file:
+        json.dump(listaLivros,json_file)
+
 
     print("Livro cadastrado com sucesso!")
+    print(listaLivros)
     perguntaCadastro = str(input("Deseja cadastrar mais algum livro? (S/N) "))
     if perguntaCadastro.upper() == "S":
         cadastroDeLivros()
 
+
 def atualizarQtdTitulo():
-    informacoesLivro.pop(5)
-    novaQuantidade = int(input("Digite a nova quantidade de unidades: "))
-    informacoesLivro.insert(5,novaQuantidade)
-    print("Alteração concluída!")
+    nomeDoLivro = str(input("informe o livro para alteração:"))
+    livroEncontrado = False
+    for i in listaLivros:
+        if i['titulo'] == nomeDoLivro:
+            novaQuantidade = int(input("Digite a nova quantidade de unidades: "))
+            i['livroUnidades'] = novaQuantidade
+
+            salvarAlteracoes()
+
+            print("Alteração concluída!")
+            livroEncontrado = True
+            break
+
+    if livroEncontrado==False:
+        print('Livro não encontrado, tente novamente!\n')
+
 
 def removerTitulos():
-    informacoesLivro.clear()
-    print("Livro removido!")
+    nomeDoLivro = input("informe o titulo do livro para excluir:")
+    livroEncontrado = False
+    for i in listaLivros:
+        if i['titulo'] == nomeDoLivro:
+            listaLivros.remove(i)
+            salvarAlteracoes()
+            print("Livro removido!")
+            livroEncontrado = True
+            break
 
+    if livroEncontrado==False:
+        print('Livro não encontrado,tente novamente!\n')
+
+
+def buscarPorExemplares():
+    exemplar = str(input("informe a informação que deseja buscar, seja ela ano,titulo,autor,tematica: "))
+    livroEncontrado = False
+    for i in listaLivros:
+        if i['titulo'] == exemplar:
+            print(i)
+            livroEncontrado = True
+            break
+
+        if i['autorLivro'] == exemplar:
+            print(i)
+            livroEncontrado = True
+            break
+
+        if i['tematica'] == exemplar:
+            print(i)
+            livroEncontrado = True
+            break
+
+        if i['anoLivro'] == exemplar:
+            print(i)
+            livroEncontrado = True
+            break
+
+    if livroEncontrado==False:
+        print("erro, tente novamente!\n")
+
+
+def reservarTitulo():
+    resposta = str(input("Digite (R) para reservar um titulo, ou (E) para remover um titulo da reserva:"))
+    nomeLivro = str(input("Digite o nome do titulo:"))
+    if resposta.upper() == "R":
+
+        for i in listaLivros:
+            if i['titulo'] == nomeLivro:
+                if i['livroAlugavel'] == "S":
+                    livrosReservados.append(i)
+
+                    with open("livrosReservados.json", "w") as json_file:
+                        json.dump(livrosReservados, json_file)
+
+                    print("titulo reservado!\n")
+                    break
+
+                if i['livroAlugavel'] == "N":
+                    print("este título não pode ser reservado!")
+                    break
+
+
+    if resposta.upper() == "E":
+        livroEncontrado = False
+        for j in livrosReservados:
+            if j['titulo'] == nomeLivro:
+                livrosReservados.remove(j)
+
+                with open("livrosReservados.json", "w") as json_file:
+                    json.dump(livrosReservados, json_file)
+
+                print("Livro retornado da reserva!\n")
+                livroEncontrado = True
+                break
+
+        if livroEncontrado==False:
+            print("livro não encontrado")
+
+
+def salvarAlteracoes():
+    with open("livros.json", "w") as write_file:
+        json.dump(listaLivros, write_file)
+
+
+def importarLivro():
+    print("--Para importar livros, certifique que o arquivo esteja na pasta do projeto--\n")
+    nomeArquivo = str(input("digite o nome do arquivo, siga o modelo (nomeDoArquivo.json): "))
+    with open(nomeArquivo, "r") as read_file:
+        dados = json.load(read_file)
+        for d in dados:
+            listaLivros.append(d)
+
+
+def statusLivro():
+    nomeLivro = str(input("informe o livro para obter status:\n"))
+    livroEncontrado = False
+
+    for j in livrosReservados:
+        if j['titulo'] == nomeLivro:
+            print("** Existe uma reserva em ativa deste livro **")
+            print("Há na biblioteca ", j['livroUnidades'], "unidades deste livro\n")
+            livroEncontrado = True
+            break
+
+    if livroEncontrado == False:
+        for i in listaLivros:
+            if i['titulo'] == nomeLivro:
+                print("Este livro não está reservado, está livre")
+                print("Há na biblioteca,", i['livroUnidades'], "unidades disponíveis")
+                livroEncontrado = True
+                break
+
+
+def relatorioAcervo():
+    relatorio = open("relatorioAcervo.txt","w")
+    relatorio.writelines("--- RELATÓRIO DO ACERVO ---\n")
+    for i in listaLivros:
+        relatorio.write("-" *40 + "\n")
+        relatorio.write("Livro: "+ i['titulo'] + "\n" +
+                        "Autor: "+ i['autorLivro'] + "\n" +
+                        "Categoria: "+ i['categoria'] + "\n" +
+                        "Tematica: "+ i['tematica'] + "\n" +
+                        "Ano do Livro: "+ i['anoLivro'] + "\n" +
+                        "Livro alugavel: "+ i['livroAlugavel'] + "\n" +
+                        "Unidades: " + i['livroUnidades']+ "\n")
+
+    print("Relatório gerado,visualize na pasta do projeto\n")
+    relatorio.close()
+
+
+def relatorioCategoria():
+    respostaCategoria = str(input("informe a categoria: "))
+    relatorio = open("relatorioPorCategoria.txt", "w")
+    relatorio.writelines("--- RELATÓRIO DA CATEGORIA:" + respostaCategoria + "---\n")
+    for i in listaLivros:
+        if i['categoria'] == respostaCategoria:
+            relatorio.write("-" * 40 + "\n")
+            relatorio.write("Livro: " + i['titulo'] + "\n" +
+                            "Autor: " + i['autorLivro'] + "\n" +
+                            "Categoria: " + i['categoria'] + "\n" +
+                            "Tematica: " + i['tematica'] + "\n" +
+                            "Ano do Livro: " + i['anoLivro'] + "\n" +
+                            "Livro alugavel: " + i['livroAlugavel'] + "\n" +
+                            "Unidades: " + i['livroUnidades'] + "\n")
+
+    print("Relatório gerado, está disponível na pasta do projeto")
+    relatorio.close()
+
+
+def relatorioTematica():
+    respostaTematica = str(input("informe a tematica: "))
+    relatorio = open("relatorioPorTematica.txt", "w")
+    relatorio.writelines("--- RELATÓRIO DA CATEGORIA:" + respostaTematica + "---\n")
+    for i in listaLivros:
+        if i['tematica'] == respostaTematica:
+            relatorio.write("-" * 40 + "\n")
+            relatorio.write("Livro: " + i['titulo'] + "\n" +
+                            "Autor: " + i['autorLivro'] + "\n" +
+                            "Categoria: " + i['categoria'] + "\n" +
+                            "Tematica: " + i['tematica'] + "\n" +
+                            "Ano do Livro: " + i['anoLivro'] + "\n" +
+                            "Livro alugavel: " + i['livroAlugavel'] + "\n" +
+                            "Unidades: " + i['livroUnidades'] + "\n")
+
+    print("Relatório gerado, está disponível na pasta do projeto")
+    relatorio.close()
--- Biblioteca/main.py
+++ Biblioteca/main.py
@@ -1,10 +1,40 @@
 from funcionalidadesBiblioteca import*
+usuario = "admin"
+senha = "admin"
+
+with open("livros.json","r") as read_file:
+    dados = json.load(read_file)
+    for d in dados:
+        listaLivros.append(d)
+
+
+with open("livrosReservados.json", "r") as read_file:
+    informacoes = json.load(read_file)
+    for i in informacoes:
+        livrosReservados.append(i)
+
+
+def login():
+    usuarioLogin = str(input("Usuario:"))
+    senhaLogin = str(input("Senha:"))
+    if usuarioLogin ==usuario and senhaLogin == senha:
+        menuSistema()
+    else:
+        print("Usuario ou senha incorreta, tente novamente!")
+        login()
+
 
 def menuSistema():
     print("* BEM VINDO AO MORAIS LIBRARY, A MELHOR BIBLIOTECA DA REGIÃO *\n")
     print("1 - DIGITE 1 PARA CADASTRAR UM LIVRO \n"
           "2 - DIGITE 2 PARA ATUALIZAR AS UNIDADES DE UM TÍTULO\n"
-          "3 - DIGITE 3 PARA REMOVER UM TÍTULO\n")
+          "3 - DIGITE 3 PARA REMOVER UM TÍTULO\n"
+          "4 - DIGITE 4 PARA BUSCAR POR EXEMPLARES\n"
+          "5 - DIGITE 5 PARA RESERVAR OU RETORNAR UM TÍTULO\n"
+          "6 - DIGITE 6 PARA OBTER STATUS DE UM LIVRO\n"
+          "7 - DIGITE 7 PARA GERAR RELATÓRIO DO ACERVO\n"
+          "8 - DIGITE 8 PARA GERAR RELATÓRIO POR CATEGORIA\n"
+          "9 - DIGITE 9 PARA GERAR RELATÓRIO POR TEMATICA\n")
     respostaMenu = str(input(""))
 
     if respostaMenu == "1":
@@ -19,8 +49,32 @@ def menuSistema():
         removerTitulos()
         menuSistema()
 
+    elif respostaMenu == "4":
+        buscarPorExemplares()
+        menuSistema()
+
+    elif respostaMenu == "5":
+        reservarTitulo()
+        menuSistema()
+
+    elif respostaMenu == "6":
+        statusLivro()
+        menuSistema()
+
+    elif respostaMenu == "7":
+        relatorioAcervo()
+        menuSistema()
+
+    elif respostaMenu == "8":
+        relatorioCategoria()
+        menuSistema()
+
+    elif respostaMenu == "9":
+        relatorioTematica()
+        menuSistema()
+
     else:
         print("Funcionalidade ainda não implementada!")
         menuSistema()
 
-menuSistema()
+login()
